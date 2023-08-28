60d86c1825998b32050367c615815f4f0601c6fc
Alberto Benatti <albi.benni8@gmail.com>
2023-08-28 21:29:06 +0200

    test the concurrent reload of tsc and nodemon

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello Worlda!"));
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
