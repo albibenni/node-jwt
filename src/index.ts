40a1e5553887da3c8317e31d50346271ceea0997
Alberto Benatti <albi.benni8@gmail.com>
2023-08-29 21:45:32 +0200

    port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => res.send("Hello Worlda!"));
 
