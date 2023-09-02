23a2934eb639ba70d6c858dc1e7db5062d9da09a
Alberto Benatti <albi.benni8@gmail.com>
2023-09-02 15:45:54 +0200

    async express for handling errors

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
+import asyncHandler from "express-async-handler";
 
-const authUser = (req: Request, res: Response) => {
+const authUser = asyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({ message: "Auth User" });
-};
+});
 
 export { authUser };
