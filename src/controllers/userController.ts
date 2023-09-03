dfd7f295088ee79b3229669d339c154edcd702f5
Alberto Benatti <albi.benni8@gmail.com>
2023-09-03 08:49:28 +0200

    error middleware in app.use

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
 
+//@desc Auth user/set tokern
+//route POST /users/login
+//@access Public
 const authUser = asyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({ message: "Auth User" });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes";
+import { notFound, errorHandler } from "./middleware/errorMiddleware";
+
 dotenv.config();
 
 const app = express();
@@ -9,5 +11,8 @@ const port = process.env.PORT || 8000;
 app.use("/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Server is Ready"));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
