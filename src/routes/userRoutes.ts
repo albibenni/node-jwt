993bcfcc91c2a6f03aec1eab474854ba687a784e
Alberto Benatti <albi.benni8@gmail.com>
2023-09-03 09:02:53 +0200

    routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,7 +1,16 @@
 import express from "express";
-import { authUser } from "../controllers/userController";
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userController";
 const userRoutes = express.Router();
 
 userRoutes.post("/auth", authUser);
+userRoutes.post("/register", registerUser);
+userRoutes.post("/logout", logoutUser);
+userRoutes.route("/profile").get(getUserProfile).put(updateUserProfile);
 
 export default userRoutes;
