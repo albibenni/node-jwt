a1668e0acd0860ed36a77b5935615ea534993ecf
Alberto Benatti <albi.benni8@gmail.com>
2023-09-03 09:03:10 +0200

    wip controllers

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -8,4 +8,38 @@ const authUser = asyncHandler(async (req: Request, res: Response) => {
   res.status(200).json({ message: "Auth User" });
 });
 
-export { authUser };
+//@desc Register a new user
+//route POST /users/register
+//@access Public
+const registerUser = asyncHandler(async (req: Request, res: Response) => {
+  res.status(200).json({ message: "Register User" });
+});
+
+//@desc Logot user
+//route POST /users/logout
+//@access Public
+const logoutUser = asyncHandler(async (req: Request, res: Response) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+//@desc Get user profile
+//route GET /users/profile
+//@access Private - Token
+const getUserProfile = asyncHandler(async (req: Request, res: Response) => {
+  res.status(200).json({ message: "User Profile" });
+});
+
+//@desc Update user profile
+//route PUT /users/profile
+//@access Private - Token
+const updateUserProfile = asyncHandler(async (req: Request, res: Response) => {
+  res.status(200).json({ message: "Update User Profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
